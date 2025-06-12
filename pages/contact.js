80d1a9f456e8afabc7270d63525b1cd002a1b432
Md Mahmudul Hasan Jisan <mdjisantahinur464078@gmail.com>
2025-06-12 23:39:45 +0300

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Contact Us</h1>
+      <p><strong>Email:</strong> jmd011852@gmail.com</p>
+      <p><strong>Phone:</strong> +966 501743662</p>
+      <p><strong>Address:</strong> RIYADH, Saudi Arabia</p>
+    </div>
+  )
+}
